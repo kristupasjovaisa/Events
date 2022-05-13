4059b4f8bd8de833bf9e92ec3dfc793c95950f7e
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-05-13 22:49:15 +0300

    Added event and user table variables

--- src/main/resources/db/db-schema.sql
+++ src/main/resources/db/db-schema.sql
@@ -3,13 +3,15 @@ DROP TABLE IF EXISTS event_entity_members;
 DROP TABLE IF EXISTS event_entity;
 CREATE TABLE event
 (
-    id                BIGINT PRIMARY KEY AUTO_INCREMENT
+    id                BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name              VARCHAR(20)    NOT NULL
 );
 
 DROP TABLE IF EXISTS user_entity;
 CREATE TABLE user
 (
-    id         BIGINT PRIMARY KEY AUTO_INCREMENT
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nickname              VARCHAR(20)    NOT NULL
 );
 
 CREATE TABLE events_users
