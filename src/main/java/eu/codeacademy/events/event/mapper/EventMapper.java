ccdf3190a50e956119bfb4437724a792e1c4e9ce
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-05-21 00:10:52 +0300

    Added mappers

--- src/main/java/eu/codeacademy/events/event/mapper/EventMapper.java
+++ src/main/java/eu/codeacademy/events/event/mapper/EventMapper.java
@@ -1,6 +1,8 @@
 package eu.codeacademy.events.event.mapper;
 
 import eu.codeacademy.events.event.dto.AddEventDto;
+import eu.codeacademy.events.event.dto.EventDto;
+import eu.codeacademy.events.event.dto.UpdateEventDto;
 import eu.codeacademy.events.event.entity.EventEntity;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,30 @@ public class EventMapper {
                 startEventDateTime(Timestamp.valueOf(dto.getStartEventDateTime())).
                 endEventDateTime(Timestamp.valueOf(dto.getEndEventDateTime())).
                 description(dto.getDescription())
+                .build();
+    }
+
+    public EventEntity mapTo(UpdateEventDto dto) {
+        return EventEntity.builder().
+                name(dto.getName()).
+                location(dto.getLocation()).
+                category(dto.getCategory()).
+                price(new BigDecimal(dto.getPrice())).
+                startEventDateTime(Timestamp.valueOf(dto.getStartEventDateTime())).
+                endEventDateTime(Timestamp.valueOf(dto.getEndEventDateTime())).
+                description(dto.getDescription())
+                .build();
+    }
+
+    public EventDto mapTo(EventEntity event) {
+        return EventDto.builder().
+                name(event.getName()).
+                location(event.getLocation()).
+                category(event.getCategory()).
+                price(String.valueOf(event.getPrice())).
+                startEventDateTime(String.valueOf(event.getStartEventDateTime())).
+                endEventDateTime(String.valueOf(event.getEndEventDateTime())).
+                description(event.getDescription())
                 .build();
     }
 }
