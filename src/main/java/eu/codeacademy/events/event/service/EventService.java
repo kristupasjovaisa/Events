ae2c826af06085e4eac5dd20c1da133af68944cf
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-05-21 00:27:07 +0300

    Implemented EventService CRUD operations

--- src/main/java/eu/codeacademy/events/event/service/EventService.java
+++ src/main/java/eu/codeacademy/events/event/service/EventService.java
@@ -3,17 +3,63 @@ package eu.codeacademy.events.event.service;
 import eu.codeacademy.events.event.dto.AddEventDto;
 import eu.codeacademy.events.event.dto.EventDto;
 import eu.codeacademy.events.event.dto.UpdateEventDto;
+import eu.codeacademy.events.event.entity.EventEntity;
+import eu.codeacademy.events.event.exception.EventNotFoundException;
+import eu.codeacademy.events.event.mapper.EventMapper;
+import eu.codeacademy.events.event.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public interface EventService {
+@Service
+@RequiredArgsConstructor
+public class EventService {
 
-    UUID add(AddEventDto dto);
+    private final EventRepository eventRepository;
+    private final EventMapper mapper;
 
-    String update(UpdateEventDto dto);
+    public EventDto add(AddEventDto dto) {
+        return mapper.mapTo(eventRepository.save(mapper.mapTo(dto)));
+    }
 
-    Long remove(Long id);
+    @Transactional
+    public boolean update(UpdateEventDto dto) {
+        Optional<EventEntity> eventOptional = eventRepository.findByEventId(dto.getEventId());
+        if (eventOptional.isPresent()) {
+            eventRepository.save(mapper.mapTo(dto));
+            return true;
+        }
+        return false;
+    }
 
-    List<EventDto> getAll();
+    @Transactional
+    public boolean delete(UUID id) {
+        Optional<EventEntity> event = eventRepository.findByEventId(id);
+        if (event.isPresent()) {
+            event.ifPresent(value -> eventRepository.deleteById(value.getId()));
+            return true;
+        }
+        return false;
+    }
+
+    public EventDto getEventByUUID(UUID id) {
+        return eventRepository.findByEventId(id).map(mapper::mapTo)
+                .orElseThrow(() -> new EventNotFoundException(id));
+    }
+
+    public List<EventDto> getAll() {
+        var list = eventRepository.findAll();
+        if (list != null) {
+            return list.stream()
+                    .map(mapper::mapTo)
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
 }
