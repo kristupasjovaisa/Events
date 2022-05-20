dbc7f32f3bca0285a0183ac5191b757cb9e7e0fe
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-05-21 00:30:14 +0300

    Changed properties type for dto models

--- src/main/java/eu/codeacademy/events/event/dto/EventDto.java
+++ src/main/java/eu/codeacademy/events/event/dto/EventDto.java
@@ -3,14 +3,18 @@ package eu.codeacademy.events.event.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 @Data
 @Builder
 public class EventDto {
 
+    private UUID eventId;
     private String name;
     private String location;
     private String category;
-    private String price;
+    private BigDecimal price;
     private String startEventDateTime;
     private String endEventDateTime;
     private String description;
