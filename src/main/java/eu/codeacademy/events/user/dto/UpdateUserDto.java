5f92f309c3eea5fb2e9f423c90c162904dd42eb1
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-05-22 14:04:13 +0300

    Added UserDto, UpdateUserDto and AddUserDto

---
+++ src/main/java/eu/codeacademy/events/user/dto/UpdateUserDto.java
@@ -0,0 +1,21 @@
+package eu.codeacademy.events.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+public class UpdateUserDto {
+
+    private Long id;
+    private UUID userId;
+    private String nickname;
+    private String location;
+    private String email;
+    private String birthday;
+    private String password;
+    private String phoneNumber;
+    private String description;
+}
