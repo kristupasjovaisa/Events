add2d976ce2442d9719c43ec78b103aba070f3df
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-06-05 19:24:20 +0300

    Added IndexController method @GetMapping("/register")

--- src/main/java/eu/codeacademy/events/index/controller/IndexController.java
+++ src/main/java/eu/codeacademy/events/index/controller/IndexController.java
@@ -1,6 +1,8 @@
 package eu.codeacademy.events.index.controller;
 
+import eu.codeacademy.events.user.dto.AddUserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +10,11 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String signUpForm(Model model, AddUserDto user){
+        model.addAttribute("user", user);
+        return "signup-form";
     }
 }
