613853c570dc3b5fbc34a7a207d3e9fc550f849d
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-06-05 22:41:16 +0300

    Delete redundat  @GetMapping("/register") method

--- src/main/java/eu/codeacademy/events/index/controller/IndexController.java
+++ src/main/java/eu/codeacademy/events/index/controller/IndexController.java
@@ -1,20 +1,13 @@
 package eu.codeacademy.events.index.controller;
 
-import eu.codeacademy.events.user.dto.AddUserDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    @GetMapping("/register")
-    public String signUpForm(Model model, AddUserDto user){
-        model.addAttribute("user", user);
-        return "signup-form";
     }
 }
