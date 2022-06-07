f99b0a0032c2d5c3824579e1252f69d499126b84
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-06-08 00:31:06 +0300

    Added function in UserRepository to query user by nickname

--- src/main/java/eu/codeacademy/events/user/repository/UserRepository.java
+++ src/main/java/eu/codeacademy/events/user/repository/UserRepository.java
@@ -2,11 +2,15 @@ package eu.codeacademy.events.user.repository;
 
 import eu.codeacademy.events.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
-
+    @Query("SELECT u FROM UserEntity u WHERE u.nickname = ?1")
+    Optional<UserEntity> findUserByNickname(String nickname);
     Optional<UserEntity> findByUserId(UUID id);
+
+
 }
