e5d8c2e5eddf663e1238811b1ebb2064c169f1cd
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-06-09 14:56:16 +0300

    Added SecurityUtils for get user id

--- src/main/java/eu/codeacademy/events/utils/SecurityUtils.java
+++ src/main/java/eu/codeacademy/events/utils/SecurityUtils.java
@@ -1,24 +1,23 @@
-//package eu.codeacademy.events.utils;
-//
-//import eu.codeacademy.events.user.dto.UserDto;
-//import eu.codeacademy.events.user.dto.UserRoleDto;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContext;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//
-//
-//public final class SecurityUtils {
-//
-//    private SecurityUtils() {
-//    }
-//
-//    public static UserDto getUser() {
-//        SecurityContext securityContext = SecurityContextHolder.getContext();
-//        Authentication authentication = securityContext.getAuthentication();
-//        if (authentication != null) {
-//            UserRoleDto userRoleDto = (UserRoleDto) authentication.getPrincipal();
-//            return userRoleDto.getUser();
-//        }
-//        return null;
-//    }
-//}
+package eu.codeacademy.events.utils;
+
+import eu.codeacademy.events.user.dto.UserDto;
+import eu.codeacademy.events.user.dto.UserRoleDto;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public final class SecurityUtils {
+
+    private SecurityUtils() {
+    }
+
+    public static UserDto getUser() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        if (authentication != null) {
+            UserRoleDto userRoleDto = (UserRoleDto) authentication.getPrincipal();
+            return userRoleDto.getUser();
+        }
+        return null;
+    }
+}
