50daefc700e47548a5beeb6201a74cea6c8b2de9
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-06-20 18:13:32 +0300

    create rest controller in api module and use it

--- Events-api/src/main/java/eu/codeacademy/events/api/event/controller/EventApiController.java
+++ Events-api/src/main/java/eu/codeacademy/events/api/event/controller/EventApiController.java
@@ -3,23 +3,22 @@ package eu.codeacademy.events.api.event.controller;
 import eu.codeacademy.events.api.event.dto.EventDto;
 import eu.codeacademy.events.api.event.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/events")
+@RestController
 public class EventApiController {
 
     private final EventService eventService;
 
     @ResponseBody
-    @GetMapping(value = "/events", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping()
     public List<EventDto> getEvents(){
         return eventService.getAllEvents();
     }
