203b3b22f314c76e7442269e310ec3936b9b14a8
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-06-22 00:21:58 +0300

    Added File Controller tag

--- Events-api/src/main/java/eu/codeacademy/events/api/file/controller/FileController.java
+++ Events-api/src/main/java/eu/codeacademy/events/api/file/controller/FileController.java
@@ -1,6 +1,7 @@
 package eu.codeacademy.events.api.file.controller;
 
 import eu.codeacademy.events.api.file.service.FileService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "File Controller")
 public class FileController {
 
     private final FileService fileService;
