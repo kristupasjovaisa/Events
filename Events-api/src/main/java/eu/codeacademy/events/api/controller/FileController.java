f1b6e89d5a7a783f3be18166767b09f3678238c9
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-06-21 20:08:46 +0300

    Added empty file controller and service with max file size config

---
+++ Events-api/src/main/java/eu/codeacademy/events/api/controller/FileController.java
@@ -0,0 +1,19 @@
+package eu.codeacademy.events.api.controller;
+
+import eu.codeacademy.events.api.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileService fileService;
+
+    @PostMapping("/api/file/upload")
+    public void saveFile(@RequestParam MultipartFile file){
+    }
+}
---
+++ Events-api/src/main/java/eu/codeacademy/events/api/service/FileService.java
@@ -0,0 +1,7 @@
+package eu.codeacademy.events.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+}
