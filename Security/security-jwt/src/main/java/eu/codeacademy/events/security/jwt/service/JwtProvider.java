91341c878208a02cb5d3a02415872e1daa40acf2
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-02 14:55:01 +0300

    Create JWT token when login (authenticate)

--- Security/security-jwt/src/main/java/eu/codeacademy/events/security/jwt/service/JwtProvider.java
+++ Security/security-jwt/src/main/java/eu/codeacademy/events/security/jwt/service/JwtProvider.java
@@ -1,11 +1,35 @@
 package eu.codeacademy.events.security.jwt.service;
 
 import eu.codeacademy.events.security.jwt.dto.UserRoleDto;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
+    private final static Date NOW = new Date();
+    @Value("#{${security.jwt.validity-time} * 60 * 1000}")
+    private long tokenValidityInMillis;
+
+    @Value("${security.jwt.secret-key}")
+    private byte[] secretKey;
     public String getToken(UserRoleDto principal) {
-        return "My token: " + principal.getUsername();
+        return Jwts.builder()
+                .setHeaderParam("typ","JWT")
+                .setIssuer("Events-api")
+                .setAudience("Events-ui")
+                .setSubject(principal.getUsername())
+                .setIssuedAt(NOW)
+                .setExpiration(new Date(NOW.getTime() + tokenValidityInMillis))
+                .claim("roles",principal.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toSet()))
+                .signWith(Keys.hmacShaKeyFor(secretKey), SignatureAlgorithm.HS512)
+                .compact();
     }
 }
