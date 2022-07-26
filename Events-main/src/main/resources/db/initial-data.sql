20e1e411f51f7e76d448cfc497912ae56b019b7c
kristupasjovaisa <kristupas.jovaisa03@gmail.com>
2022-07-26 15:41:08 +0300

    Renamed events category properties

--- Events-main/src/main/resources/db/initial-data.sql
+++ Events-main/src/main/resources/db/initial-data.sql
@@ -8,13 +8,13 @@ VALUES ('e4dbc123-a7c2-4bee-a519-e1b9ba991341', 'ADMIN', 'Panevezys', 'af@gmail.
 
 INSERT INTO events(event_id, name, location, category, price, start_event_date_time, end_event_date_time, description,
                    owner_id)
-VALUES ('e4dbc123-a7c2-4bee-a519-e1b9ba991344', 'Pirmas', 'moletai', 'Sports', 1599.99, '2007-04-04T20:20:00',
+VALUES ('e4dbc123-a7c2-4bee-a519-e1b9ba991344', 'Pirmas', 'moletai', 'Sportas', 1599.99, '2007-04-04T20:20:00',
         '2007-05-04T20:20:00',
         'smagu', 1),
-       ('e4dbc123-a7c2-4bee-a519-e1b9ba991345', 'Antras', 'jurbarkas', 'Festivals', 1000.99, '2008-04-04T20:21:00',
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991345', 'Antras', 'jurbarkas', 'Festivalis', 1000.99, '2008-04-04T20:21:00',
         '2008-05-04T20:21:00',
         'labai', 2),
-       ('e4dbc123-a7c2-4bee-a519-e1b9ba991346', 'Trecias', 'zarasai', 'Concerts', 10.99, '2009-05-04T20:22:00',
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991346', 'Trecias', 'zarasai', 'Koncertas', 10.99, '2009-05-04T20:22:00',
         '2009-05-04T20:22:00',
         'buvo', 3);
 
